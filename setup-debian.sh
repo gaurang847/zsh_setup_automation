7c8274b78f356f93633f6bba6c5518c8e7cd5ee9
Gaurang Pansare <gaurangpansare@yahoo.in>
2022-01-27 02:12:31 +0530

    Added step in ZSH setup for debian systems to install nerd-font patch for the Fira Code font

--- setup-debian.sh
+++ setup-debian.sh
@@ -12,6 +12,15 @@ print_heading () {
 
 print_heading "Step 0: Preparation"
 sudo apt update   # Update apt cache
+sudo apt install -y git
 
 print_heading "Step 1: Installing Font FiraCode"
 sudo apt install -y fonts-firacode
+
+print_heading "Step 2: Installing Nerd-Font patch for the FiraCode font"
+if [[ ! -d /tmp/nerd-fonts ]]
+then
+  git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git /tmp/nerd-fonts
+fi
+cd /tmp/nerd-fonts
+./install.sh FiraCode
